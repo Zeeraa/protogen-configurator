48453471d0acc06d2c4d93e25d1b822e0b2efef9
Anton Udén <udenanton@hotmail.com>
2026-06-01 23:23:59 +0200

    add about page and prepare for initial release

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { FaceEditorPage } from './features/pages/face-editor-page/face-editor-pa
 import { FacesPage } from './features/pages/faces-page/faces-page';
 import { MatrixConfigPage } from './features/pages/matrix-config-page/matrix-config-page';
 import { unsavedChangesGuard } from './core/guards/unsaved-changes.guard';
+import { AboutPage } from './features/pages/about-page/about-page';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'config', pathMatch: 'full' },
-  { path: 'config', component: ConfigPage, title: "Config", canDeactivate: [unsavedChangesGuard] },
-  { path: 'faces', component: FacesPage, title: "Faces" },
-  { path: 'faces/:uuid', component: FaceEditorPage, title: "Edit Face", canDeactivate: [unsavedChangesGuard] },
-  { path: 'matrix-config', component: MatrixConfigPage, title: "Matrix Config", canDeactivate: [unsavedChangesGuard] },
-  { path: 'export', component: ExportPage, title: "Export" },
+  { path: '', component: AboutPage, title: "About - Protogen Configurator" },
+  { path: 'config', component: ConfigPage, title: "Config - Protogen Configurator", canDeactivate: [unsavedChangesGuard] },
+  { path: 'faces', component: FacesPage, title: "Faces - Protogen Configurator" },
+  { path: 'faces/:uuid', component: FaceEditorPage, title: "Edit Face - Protogen Configurator", canDeactivate: [unsavedChangesGuard] },
+  { path: 'matrix-config', component: MatrixConfigPage, title: "Matrix Config - Protogen Configurator", canDeactivate: [unsavedChangesGuard] },
+  { path: 'export', component: ExportPage, title: "Export - Protogen Configurator" },
 ];
